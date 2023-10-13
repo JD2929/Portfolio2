9fbc2d8b33d8b6b3ed366339198b7851fca4df57
JD2929 <jandunk@creonova.com>
2023-10-12 21:44:29 -0300

    Added component and style folders and files.

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import '../styles/header.css';
+
+function Header () {
+    return(
+        <header>
+            <h1>JD</h1>
+<ul>
+    <li></li>
+</ul>
+        </header>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css'; // check to make sure this is correct
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
